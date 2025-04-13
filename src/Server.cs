a4d2ace11d0390160a6fc439ba670a81f3222652
arpitsirohi <sirohi.arpit18@gmail.com>
2025-04-13 18:51:09 +0530

    Changes for user agent

--- src/Server.cs
+++ src/Server.cs
@@ -28,9 +28,22 @@ var requestLines = request.Split("\r\n");
 var requests = requestLines[0].Split("/");
 var linesPart = requestLines[0].Split(' ');
 var (requestMethod, path, httpVersion) = (linesPart[0], linesPart[1], linesPart[2]);
-var content= linesPart[1].Contains("/echo/") ? linesPart[1].Substring(linesPart[1].LastIndexOf("/")+1) : null; 
-string response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" :
-    path.Contains("/echo/") ? $"{httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}":$"{httpVersion} 404 Not Found\r\n\r\n";
+var content= linesPart[1].Contains("/echo/") ? linesPart[1].Substring(linesPart[1].LastIndexOf("/")+1) : linesPart[1].Contains("/user-agent")
+    ? linesPart[1].Substring(linesPart[1].LastIndexOf("/") + 1) :null;
+String response;
+if (content.Equals("user-agent", StringComparison.CurrentCultureIgnoreCase))
+{
+    var useragentIndex = requestLines[3].ToLower().IndexOf("user-agent");
+    var useragent = requestLines[3].ToLower().Remove(useragentIndex, 11).Trim();
+    var useragentLength = useragent.Length;
+    response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" :
+        path.Contains("/user-agent") ? $"{httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {useragentLength}\r\n\r\n{useragent}" : $"{httpVersion} 404 Not Found\r\n\r\n";
+}
+else
+{
+    response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" :
+        path.Contains("/echo/") ? $"{httpVersion} 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content.Length}\r\n\r\n{content}" : $"{httpVersion} 404 Not Found\r\n\r\n";
+}
 byte[] responseBytes = Encoding.ASCII.GetBytes(response);
 stream.Write(responseBytes, 0, responseBytes.Length);
 
