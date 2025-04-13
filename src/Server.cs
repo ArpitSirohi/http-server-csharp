3513a85529d1a93c3cd6a96c80dceedc3a7bc15e
arpitsirohi <sirohi.arpit18@gmail.com>
2025-04-13 20:32:10 +0530

    Correction to ignore casing test

--- src/Server.cs
+++ src/Server.cs
@@ -31,13 +31,14 @@ var (requestMethod, path, httpVersion) = (linesPart[0], linesPart[1], linesPart[
 var content= linesPart[1].Contains("/echo/") ? linesPart[1].Substring(linesPart[1].LastIndexOf("/")+1) : linesPart[1].Contains("/user-agent", StringComparison.CurrentCultureIgnoreCase)
     ? linesPart[1].Substring(linesPart[1].LastIndexOf("/") + 1) :null;
 String response;
-if (content.Equals("user-agent", StringComparison.CurrentCultureIgnoreCase))
+if (!string.IsNullOrWhiteSpace(content) &&content.Equals("user-agent", StringComparison.CurrentCultureIgnoreCase))
 {
     Console.WriteLine("requestline 0 : - {0}", requestLines[0]);
     Console.WriteLine("requestline 1 : - {0}", requestLines[1]);
     Console.WriteLine("requestline 2 : - {0}", requestLines[2]);
     Console.WriteLine("requestline 3 : - {0}", requestLines[3]);
    // var useragentIndex = requestLines[3].ToLower().IndexOf("user-agent");
+
     var useragent = requestLines[2].ToLower().Remove(0, 11).Trim();
     int useragentLength = useragent.Length;
     response = path == "/" ? $"{httpVersion} 200 OK\r\n\r\n" :
