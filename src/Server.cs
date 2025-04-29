103156b018cf92ce94923bb631519da5562edf6f
arpitsirohi <sirohi.arpit18@gmail.com>
2025-04-30 02:26:57 +0530

    changed processing for content

--- src/Server.cs
+++ src/Server.cs
@@ -52,7 +52,7 @@ while (connectionisOn)
         Console.WriteLine("Started POST Response");
         Console.WriteLine("Started response for server");
         //Console.WriteLine("Content is as follows {0}",requestLines[7]);
-        var (contentType, contentLength,contentData ) = (requestLines[5], requestLines[4].ElementAt(requestLines[4].Length - 1), body);
+        var contentData = (body);
             var newfilePath = Path.Join(args[1], linesPart[1].Substring(1));
         try
         {
